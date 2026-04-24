639879c15c7e8dd5d2e2e4196f55c661802c63ad
minifish930 <97420020+minifish930@users.noreply.github.com>
2026-04-24 21:25:22 +0800

    Update geojsonloader.js

--- assets/geojsonloader.js
+++ assets/geojsonloader.js
@@ -180,11 +180,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     {
-      groupName: "火山地震与热点",
+      groupName: "海底基础信息",
       layers: [
         { name: "火山 volcanos", file: "volcanos.json" },
+        { name: "地震 10-23_5 )", file: "earthquake10_23_5.json" },
         { name: "热点 hotspots", file: "hotspots.json" },
         { name: "大火成岩省 (Johansson)", file: "LIP_Johansson.json" },
+        { name: "洋壳年龄30Ma", file: "seafloor_age_30.geojson" },
+        { name: "洋壳年龄5Ma", file: "seafloor_age_5.geojson" },
+
       ],
     },
 
